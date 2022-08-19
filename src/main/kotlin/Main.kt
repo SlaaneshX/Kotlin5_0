705443d49a4447f76529ffbb1e210c1ced9ea25d
Alexander <tumakov.a@yandex.ru>
2022-08-19 10:23:17 +0300

    classes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,39 @@
+import java.util.Objects
+
 fun main() {
 
+}
+
+//Data класс Post (и другие классы, которые могут быть вложены в Post).
+data class Post(
+    val id: Int,
+    val ownerId: Int,
+    val fromId: Int,
+    val createdBy: Int,
+    val date: Int,
+    val text: String,
+    val replyOwnerId: Int,
+    val replyPostId: Int,
+    val friendsOnly: Boolean,
+    var comments: Comments = Comments(),
+    var likes: Likes = Likes()
+)
+
+data class Comments(
+    var count: Int = 0,
+    var canPost: Boolean = true,
+    var groupsCanPost: Boolean = true,
+    var canClose: Boolean = true,
+    var canOpen: Boolean = true
+)
+
+data class Likes(
+    var count: Int = 0,
+    var userLikes: Boolean = true,
+    var canLike: Boolean = true,
+    var canPublish: Boolean = true
+)
+
+object WallService {
+//Объект WallService, который внутри себя хранит посты в массиве.
 }
